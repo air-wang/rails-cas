b22b16d1d3d4a9613f77e94ebc1ad4c2119a3a95
air_wang <air@whit.ah.cn>
2016-11-19 17:29:45 +0800

    I18n init

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,6 @@ module RailsCas
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = 'zh-CN'
-    config.i18n.available_locales = ['zh-CN', 'en']
+    config.i18n.available_locales = ['zh-CN', 'en', 'zh-TW']
   end
 end
